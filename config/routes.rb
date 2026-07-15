6981720c11b7c6e4f67becda3f73b2d2ebd087e1
Yassir Khalid <y@ssir.net>
2026-07-15 09:27:23 +0200

    Add venues and queue_entries routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root "venues#index"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :venues, only: [:index, :show] do
+    member do
+      get :qr_code
+    end
+    resource :queue_entry, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :queue_entries, only: [:show] do
+    member do
+      get :confirmation
+      patch :confirm
+      patch :decline
+    end
+  end
+
+  get "manager", to: "manager#show"
 end
