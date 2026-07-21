579ba72b67ab80e849e24d9d6a39ce6a38869b1e
Nicolas Beladen <nicolas@beladen.fr>
2026-07-21 09:23:26 +0200

    Add venue_qr_code_tag helper

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,6 +1,7 @@
 module VenuesHelper
   def venue_qr_code_tag(venue)
     RQRCode::QRCode.new(venue_url(venue)).as_svg(
+      color: "16324D", # $brand — cf. config/_colors.scss
       module_size: 6,
       standalone: true,
       use_path: true
