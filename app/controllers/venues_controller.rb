bd5d8729fb5b2230a31e2ff1339ffa6682c82d78
Nicolas Beladen <nicolas@beladen.fr>
2026-07-21 08:43:50 +0200

    Add qr_code action to venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,17 +1,21 @@
 class VenuesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show qr_code]
+  before_action :set_venue, only: %i[show qr_code]
 
   def index
     @venues = Venue.search(params[:query])
   end
 
   def show
-    @venue = Venue.find(params[:id])
     @queue_count = @venue.current_queue_count
   end
 
   def qr_code
+  end
+
+  private
+
+  def set_venue
     @venue = Venue.find(params[:id])
-    render :show
   end
 end
