3f4476c233d39825d0a058610370bf0cb7fbd1a3
Nicolas Beladen <nicolas@beladen.fr>
2026-07-24 09:33:02 +0200

    Add geolocation mode to venues index (#67)

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,15 +2,23 @@ class VenuesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show qr_code]
   before_action :set_venue, only: %i[show qr_code]
 
+  # Critères de filtrage de la liste de venues
+  # 3 mode de recherche : nearby, geolocated et search
   def index
     if params[:near_venue_id].present?
       @mode            = :nearby
       @reference_venue = Venue.find(params[:near_venue_id])
       @venues          = Venue.nearby_of(@reference_venue).to_a
+      @filter_params   = { near_venue_id: @reference_venue.id }
+    elsif coordinates.present?
+      @mode          = :geolocated
+      @venues        = Venue.nearby(*coordinates).to_a
+      @filter_params = { latitude: coordinates.first, longitude: coordinates.last }
     else
-      @mode   = :search
-      @query  = params[:query]
-      @venues = Venue.search(@query)
+      @mode          = :search
+      @query         = params[:query]
+      @venues        = Venue.search(@query)
+      @filter_params = { query: @query }
     end
   end
 
@@ -25,5 +33,15 @@ class VenuesController < ApplicationController
 
   def set_venue
     @venue = Venue.find(params[:id])
+  end
+
+  # Coordonnées envoyées par le bouton de géolocalisation.
+  # nil si absentes ou non numériques : on retombe alors sur la recherche texte.
+  def coordinates
+    latitude  = Float(params[:latitude], exception: false)
+    longitude = Float(params[:longitude], exception: false)
+    return nil unless latitude && longitude
+
+    [latitude, longitude]
   end
 end
